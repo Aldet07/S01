bb71c13a8974ef28e543cb941a1c127abc101015
João Vitor Souza Aldet <24684174+Aldet07@users.noreply.github.com>
2025-08-28 22:38:43 -0300

    Update Relatório 2: Lua - Exercício 2.lua

--- Relatório 2: Lua - Exercício 2.lua
+++ Relatório 2: Lua - Exercício 2.lua
@@ -1,12 +1,23 @@
 function maiorValor(tabela)
-    local maior = tabela[1]
-    for i = 2, #tabela do
-        if tabela[i] > maior then
-            maior = tabela[i]
-        end
-    end
-    return maior
+local maior = tabela[1]
+for i = 2, #tabela do
+if tabela[i] > maior then
+maior = tabela[i]
+end
+end
+return maior
 end
 
-local numeros = {5, 9, 3, 12, 7}
-print(maiorValor(numeros))
+
+local numeros1 = {}
+io.write("Quantidade: ")
+local qtd1 = tonumber(io.read())
+
+
+for i = 1, qtd1 do
+io.write("Numero " .. i .. ": ")
+numeros1[i] = tonumber(io.read())
+end
+
+
+print("Maior: " .. maiorValor(numeros1))
