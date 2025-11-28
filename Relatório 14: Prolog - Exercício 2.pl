aa965fa183baa9d9db5e29eaef5b26764be515c9
João Vitor Souza Aldet <24684174+Aldet07@users.noreply.github.com>
2025-11-28 01:26:12 -0300

    Add Prolog definitions for Greek deities

---
+++ Relatório 14: Prolog - Exercício 2.pl
@@ -0,0 +1,34 @@
+filho(zeus, cronos).
+filho(hades, cronos).
+filho(posseidon, cronos).
+filho(hera, cronos).
+filho(demeter, cronos).
+filho(hestia, cronos).
+
+mae(zeus, reia).
+mae(hades, reia).
+mae(posseidon, reia).
+mae(hera, reia).
+mae(demeter, reia).
+mae(hestia, reia).
+
+dominio(zeus, ceu).
+dominio(zeus, trovao).
+dominio(posseidon, mar).
+dominio(posseidon, terremoto).
+dominio(hades, submundo).
+
+habitar(zeus, olimpo).
+habitar(posseidon, olimpo).
+habitar(hades, submundo).
+
+divindade_olimpica(Deus) :-
+    filho(Deus, cronos),
+    dominio(Deus, Dominio),
+    member(Dominio, [ceu, mar, submundo]).
+
+deus_maior(Deus) :-
+    habitar(Deus, olimpo),
+    findall(Dom, dominio(Deus, Dom), L),
+    length(L, N),
+    N >= 2.
