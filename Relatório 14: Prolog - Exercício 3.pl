174a7e2f15b13ced998c5a712481ade74e8b307f
João Vitor Souza Aldet <24684174+Aldet07@users.noreply.github.com>
2025-11-28 01:36:40 -0300

    Refactor Prolog code by removing unused predicates
    Removed definitions of domains and habitats for gods.

--- Relatório 14: Prolog - Exercício 3.pl
+++ Relatório 14: Prolog - Exercício 3.pl
@@ -12,27 +12,6 @@ mae(hera, reia).
 mae(demeter, reia).
 mae(hestia, reia).
 
-dominio(zeus, ceu).
-dominio(zeus, trovao).
-dominio(posseidon, mar).
-dominio(posseidon, terremoto).
-dominio(hades, submundo).
-
-habitar(zeus, olimpo).
-habitar(posseidon, olimpo).
-habitar(hades, submundo).
-
-divindade_olimpica(Deus) :-
-    filho(Deus, cronos),
-    dominio(Deus, Dominio),
-    member(Dominio, [ceu, mar, submundo]).
-
-deus_maior(Deus) :-
-    habitar(Deus, olimpo),
-    findall(D, dominio(Deus, D), L),
-    length(L, N),
-    N >= 2.
-
 irmaos_germanos(A, B) :-
     filho(A, P),
     filho(B, P),
