7c2dc33e693ef0132c7ab0a625922191d41df332
João Vitor Souza Aldet <24684174+Aldet07@users.noreply.github.com>
2025-11-28 01:35:54 -0300

    Remove redundant family relations from Prolog file

--- Relatório 14: Prolog - Exercício 2.pl
+++ Relatório 14: Prolog - Exercício 2.pl
@@ -1,17 +1,3 @@
-filho(zeus, cronos).
-filho(hades, cronos).
-filho(posseidon, cronos).
-filho(hera, cronos).
-filho(demeter, cronos).
-filho(hestia, cronos).
-
-mae(zeus, reia).
-mae(hades, reia).
-mae(posseidon, reia).
-mae(hera, reia).
-mae(demeter, reia).
-mae(hestia, reia).
-
 dominio(zeus, ceu).
 dominio(zeus, trovao).
 dominio(posseidon, mar).
@@ -22,13 +8,8 @@ habitar(zeus, olimpo).
 habitar(posseidon, olimpo).
 habitar(hades, submundo).
 
-divindade_olimpica(Deus) :-
-    filho(Deus, cronos),
-    dominio(Deus, Dominio),
-    member(Dominio, [ceu, mar, submundo]).
-
 deus_maior(Deus) :-
     habitar(Deus, olimpo),
-    findall(Dom, dominio(Deus, Dom), L),
+    findall(D, dominio(Deus, D), L),
     length(L, N),
     N >= 2.
